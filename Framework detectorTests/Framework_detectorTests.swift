2045438e20a018f34b65de9fc95bafddaa59af0b
Seamain <quanminjun37@gmail.com>
2026-06-12 09:43:06 +0800

    Initial Commit

---
+++ Framework detectorTests/Framework_detectorTests.swift
@@ -0,0 +1,19 @@
+//
+//  Framework_detectorTests.swift
+//  Framework detectorTests
+//
+//  Created by 张烨轩 on 2026/6/12.
+//
+
+import Testing
+@testable import Framework_detector
+
+struct Framework_detectorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
